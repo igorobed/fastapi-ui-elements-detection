0407794b32a47dad2cc3e74cdad64673bb99e0ef
DungeonMaster3000 <igorobed@yandex.ru>
2023-04-06 01:09:29 +0300

    Add detection close item

--- src/burger_menu/detector.py
+++ src/burger_menu/detector.py
@@ -12,9 +12,9 @@ from .utils import (
 class DetectorBurgerMenu:
     def __init__(self) -> None:
         self.model = Yolov5(
-            classes="menu",
-            backend="onnx",
-            weight="models/best.onnx",
+            classes=["menu", "close"],
+            backend="torch",
+            weight="models/best_menu_close.torchscript",
             device="cpu",
             input_shape=640,
         )
@@ -28,12 +28,18 @@ class DetectorBurgerMenu:
         # то просто возвращаем исходный объект
 
         found_elements = {}
+        # необходимо вернуть максимальный менюшный элемент
+        # необходимо вернуть все найденные крестики
 
-        if len(preds) > 0:
+        menu_items = [item for item in preds if item.class_name == "menu"]
+        close_items = [item for item in preds if item.class_name == "close"]
+
+        if len(menu_items) > 0:
+
             # модель может найти несколько элементов похожих на меню
             # необходимо выбрать наиболее вероятный
-            max_conf = max([item.confidence for item in preds])
-            for pred in preds:
+            max_conf = max([item.confidence for item in menu_items])
+            for pred in menu_items:
                 if pred.confidence == max_conf:
                     best_pred = pred
                     break
@@ -43,13 +49,24 @@ class DetectorBurgerMenu:
             # иначе у нас произошло ложноположительное срабатывание
             if best_pred.bbox[1] < 2 * (img.shape[0] / 3):
                 found_elements["burger_menu"] = {
-                    "top_left": best_pred.bbox[:2],
-                    "bottom_right": best_pred.bbox[2:],
+                    "top_left": list(map(int, best_pred.bbox[:2])),
+                    "bottom_right": list(map(int, best_pred.bbox[2:])),
                 }
                 draw_bbox(img, best_pred.bbox, color=(0, 0, 255))
         else:
             # my_logger.info("The model did not find the desired object")
-            pass
+            found_elements["burger_menu"] = {}
+        
+        found_elements["close"] = []
+        if len(close_items) > 0:
+            for pred in close_items:
+                found_elements["close"].append(
+                    {
+                    "top_left": list(map(int, pred.bbox[:2])),
+                    "bottom_right": list(map(int, pred.bbox[2:])),
+                    }
+                    )
+                draw_bbox(img, pred.bbox, color=(0, 0, 255))
 
         img = convert_from_cv2_to_image(img)
 
